80ab85303e5a20aabb8813a9b504f1f923a0f5f1
Cedric <Cedric>
2026-04-01 16:05:42 +0200

    fix cors

--- api/SendMail.js
+++ api/SendMail.js
@@ -3,7 +3,8 @@ import nodemailer from "nodemailer";
 export default async function handler(req, res) {
 
 // CORS
-res.setHeader("Access-Control-Allow-Origin", "*");
+// res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Origin", "https://ptr-eosin.vercel.app")
 res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
 res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
