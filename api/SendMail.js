646093c2394d9dd20ebb00f66ef71309f3ff1907
Cedric <Cedric>
2026-04-01 15:02:42 +0200

    Ajout de nouveau fichier

---
+++ api/SendMail.js
@@ -0,0 +1,50 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+
+if (req.method !== "POST") {
+return res.status(405).json({ message: "Method not allowed" });
+}
+
+const { name, email, subject, message } = req.body;
+
+try {
+
+const transporter = nodemailer.createTransport({
+
+service: "gmail",
+
+auth: {
+user: process.env.EMAIL_USER,
+pass: process.env.EMAIL_PASS
+}
+
+});
+
+await transporter.sendMail({
+
+from: process.env.EMAIL_USER,
+
+to: "atebacedric06@gmail.com",
+
+subject: subject,
+
+text: `
+Nom: ${name}
+Email: ${email}
+
+Message:
+${message}
+`
+
+});
+
+return res.status(200).json({ success: true });
+
+} catch (error) {
+
+return res.status(500).json({ error: error.message });
+
+}
+
+}
