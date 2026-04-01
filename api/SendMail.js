0a2482cd23ee8fd4abd85d880281b201f954c44b
Cedric <Cedric>
2026-04-01 16:00:25 +0200

    Ajout de nouveau fichiers

--- api/SendMail.js
+++ api/SendMail.js
@@ -2,6 +2,11 @@ import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
 
+// CORS
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
 if (req.method !== "POST") {
 return res.status(405).json({ message: "Method not allowed" });
 }
